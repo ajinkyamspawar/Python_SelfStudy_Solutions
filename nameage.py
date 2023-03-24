214b1e35e70987b98e5fa46d950eb5b7988b2347
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-24 16:01:10 +0530

    Update nameage.py

--- nameage.py
+++ nameage.py
@@ -1,3 +1,5 @@
 name = input("Enter your name")
 age = int(input("Enter your age"))
 print("Hi",name,"you are",age,"years old.")
+
+#to run the same file using shell exec(open(r"C:\Users\AJINKYA\AppData\Local\Programs\Python\Python311\nameage.py").read())
