1eb9c1cf3c216a9990b7df11859ab30a98ef7bbf
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-24 17:57:22 +0530

    Add files via upload

--- nameage.py
+++ nameage.py
@@ -2,4 +2,7 @@ name = input("Enter your name")
 age = int(input("Enter your age"))
 print("Hi",name,"you are",age,"years old.")
 
-#to run the same file using shell exec(open(r"C:\Users\AJINKYA\AppData\Local\Programs\Python\Python311\nameage.py").read())
+X = ('Hello' 'World')
+print(X, end='!')
+
+print('\nAise bhi hota hai...', end = '@')
