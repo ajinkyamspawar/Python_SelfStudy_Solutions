aacee05a25d9a903ebc437fa24e908c91c904e0c
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-24 18:08:14 +0530

    Add files via upload

---
+++ Assignment2_Q1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 18:00:33 2023
+
+@author: AJINKYA
+
+"""
+
+#Q1 Swap values of 3 variable without taking a temporary variable 
+# Assign 3 values to 3 different variables
+a = 10
+b = 20
+c = 30
+print("Values before swapping:","a=",a,"b=",b,"c=",c)
+#Swapping the values of these three variables 
+
+a,b,c = c,a,b
+
+print("Values before swapping:","a=",a,"b=",b,"c=",c)
