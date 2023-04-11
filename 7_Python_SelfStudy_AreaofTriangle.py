a14feb9626e9a5633768b34648cd00a1422f699f
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-11 15:54:05 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_AreaofTriangle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 15:51:08 2023
+
+@author: AJINKYA
+"""
+
+#Finding areas of geometrical figures 
+
+h = float(input("Enter height:"))
+
+b = float(input("Enter breadth"))
+
+areaoftriangle = 0.5*h*b
+
+print(areaoftriangle)
