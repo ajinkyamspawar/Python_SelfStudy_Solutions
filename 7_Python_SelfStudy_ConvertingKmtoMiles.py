7ea56138918eb982c13219df1c5f56ec168707b3
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-11 16:00:23 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_ConvertingKmtoMiles.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 15:57:00 2023
+
+@author: AJINKYA
+"""
+#Converting km to miles
+km = float(input("Enter km to convert in miles:"))
+
+miles = (0.621371) * km 
+
+print(km , "Kms will be", miles, "miles.")
+
