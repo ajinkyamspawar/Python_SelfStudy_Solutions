5542fd7320305ec125f06fb957333eaa18ba0db2
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-11 15:44:04 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_LocalvsPassedvariableValue.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 15:39:15 2023
+
+@author: AJINKYA
+"""
+#Local vs Passed variable value
+def fun(x,y):
+    x = 1
+    y = 1
+    
+    return x+y
+
+print(fun(3,4))
+
+#Output is 2 and not 7
