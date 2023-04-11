068d06bf633842be7c89a8aec97af3ad72940a41
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-11 17:29:30 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_RandomNumberGeneration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 17:25:03 2023
+
+@author: AJINKYA
+"""
+
+import random
+
+number = random.randint(1, 6)
+
+number2 = random.random()
+
+print(number)
+
+print(number2)
