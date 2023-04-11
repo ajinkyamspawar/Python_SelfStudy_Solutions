ad42bf21f275c8636b29b9659b330f686f45d05a
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-11 16:08:25 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_OddEven.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 16:04:28 2023
+
+@author: AJINKYA
+"""
+
+number = int(input("Enter a number:"))
+
+if number%2 == 0:
+    print("Even number")
+else: 
+    print("odd number")
