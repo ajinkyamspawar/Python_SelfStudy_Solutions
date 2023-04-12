277e9d0be13fe567a2e909951bb76bed557934a4
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-12 09:10:07 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_ListElementsAddition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 08:34:20 2023
+
+@author: AJINKYA
+"""
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+length_list1 = len(list1)
+
+for i in range(length_list1-1):
+   print(list1[i] + list1[i+1]) 
