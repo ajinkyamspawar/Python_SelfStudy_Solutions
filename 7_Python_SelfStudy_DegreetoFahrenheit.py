bd4c3d7be2aec1ba54e99f7a62f7221348d36b7e
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-12 13:07:20 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_DegreetoFahrenheit.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 12:59:25 2023
+
+@author: AJINKYA
+"""
+#To convert degree to farenheit
+celsius = float(input("Enter temperature value to convert in fahrenheit"))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print(fahrenheit)
+
+
+#To convert fahrenheit to degree
+
+f = float(input("Enter temperature value to convert in celcius"))
+
+c = (f-32) / 1.8
+
+print(c)
+
