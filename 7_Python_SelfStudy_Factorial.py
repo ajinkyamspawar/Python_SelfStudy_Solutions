476b6c53be9a81a5b9f9a3cd91cb8021269a214e
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-12 13:37:52 +0530

    Add files via upload

--- 7_Python_SelfStudy_Factorial.py
+++ 7_Python_SelfStudy_Factorial.py
@@ -12,12 +12,18 @@ number = int(input("Enter a number to get it's factorial"))
 # Intializing fact variable at 1 to traverse till the number 
 fact = 1
 
+if number < 0:
+    raise Exception("Factorial of 0 does not exist")
+
+if number == 0:
+    print("Factorial of 0 is 1")
 # looping through the range
-for i in range(1, number+1):
-    fact = fact * i
+if number > 0:
+    for i in range(1, number+1):
+        fact = fact * i
 
 #Printing factorial
-print(fact)
+    print(fact)
 
 
 # One more method 
@@ -25,12 +31,19 @@ print(fact)
 
 number = int(input("Enter a number to get it's factorial"))
 
-
 fact = 1
 
-while i !=0:
-    fact = fact * number
-    number -= 1
+if number < 0:
+    raise Exception("Factorial of negative does not exist")
+
+if number == 0:
+    print("Factorial of 0 is 1")
+    
+# looping through the range
+
+while number !=0:
+        fact = fact * number
+        number -= 1
 print(fact)
 
 
