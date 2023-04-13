d75f3210d8e3699d60a84bec514c3fc3c01f546f
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-13 12:55:41 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_FindingASCIIvalue.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 12:50:42 2023
+
+@author: AJINKYA
+"""
+#Find ASCII value of chanracter 
+#American standard code for information interchange
+#Method 1: using inbuilt function, ord() finction is used 
+
+char = "z"
+
+print("ASCII values of", char, "is", ord(char))
