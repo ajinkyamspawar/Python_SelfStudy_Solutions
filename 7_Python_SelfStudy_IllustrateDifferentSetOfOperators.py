cae5cf4c104a7bb0fabbfdb8f0872dd3506c3d93
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-15 17:49:27 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_IllustrateDifferentSetOfOperators.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 17:44:31 2023
+
+@author: AJINKYA
+"""
+
+A = {1,2,3,4,5,6}
+
+B = {5,6,7,8,9,10}
+
+print("The union is", A | B)
+
+print("The intersection is", A & B)
+
+print("The difference is", A - B)
+
+print("The symmetric difference is",A ^ B)
+
