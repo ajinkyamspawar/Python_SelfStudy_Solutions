488de694d46780ff7839a0b12cfb3552228e96f8
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-17 19:35:20 +0530

    Add files via upload

---
+++ 7_Python_CheckKeyPresentinDict.py
@@ -0,0 +1,28 @@
+#Check key present in dict or not
+#Check key present or not
+dict = {1:"Yudhisthir", 2:"Bhim", 3:"Arjun",4:"Nakul", 5:"Sahdev"}
+
+name = int(input("Enter a key here"))
+
+if name in dict.keys():
+    print("Key is present")
+else:
+    print("Key is not present")
+
+#Check value present or not
+
+
+#Check key present in dict or not
+#Check key present or not
+dict = {1:"Yudhisthir", 2:"Bhim", 3:"Arjun",4:"Nakul", 5:"Sahdev"}
+
+name = input("Enter a value here")
+
+if name in dict.values():
+    print("value is present")
+else:
+    print("Value is not present")
+
+
+
+
