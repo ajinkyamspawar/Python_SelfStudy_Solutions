328f51f149f3916081c53a6de507c56d72b1caca
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-17 15:24:08 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_Slicing.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 14:51:29 2023
+
+@author: AJINKYA
+"""
+
+list = ["Ram", "Krishna", "Vasudev", "Arjun", "Yudhisthir", "Bhim","Nakul","Sahdev"]
+
+print(list[0]) #output Ram
+print(list[-8]) #output Ram
+
+print(list[2:7]) #Vausedv Arjun Yudhisthir bhim Nakul Sahdec
+
+print(list[-1:]) #only Sahdev printed
+
+print(list[::-1]) #list reverse
+
+print(list[:3])
+
+print(list[::2]) # with  a gap of 2 values will be displayed as Ram Arjun Nakul
+
+print(list[-7:-2])
