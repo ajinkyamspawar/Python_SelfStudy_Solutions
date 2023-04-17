be7aa185f2eeaaa7222e2dec8936e9c08fd0b9db
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-17 12:27:41 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_NestedDirectory.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 12:16:42 2023
+
+@author: AJINKYA
+"""
+
+from pathlib import Path
+Path("D:/1_AJINKYA_PAWAR/3_SOFTWARE JOB/1_CDAC/1_CCEE_PLACEMENT/4_PYTHONRP/new_dict").mkdir(parents = True, exist_ok = True)
