154d1d886d052f1d94a77ba218e8d851d930a843
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-18 09:44:45 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_FindingSubstring.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 09:29:11 2023
+
+@author: AJINKYA
+"""
+#Printing substring from string
+string = "Harry Potter and the Goblet of Fire"
+
+print(string[0:12])
+
+print(string[-15:-7])
