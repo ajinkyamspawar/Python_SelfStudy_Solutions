14d5b26c31accd861aed7fb9e1ee94338844c19e
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-18 09:48:32 +0530

    Add files via upload

---
+++ 7_Python_SelfStudy_PrintingaSingleLine.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 09:46:45 2023
+
+@author: AJINKYA
+"""
+
+#Printing a single line output
+print("Harry Potter")
+print("and the Goblet of fire")
+
+print("Harry Potter", end = " ")
+print("and the Prisoner of Azkaban")
