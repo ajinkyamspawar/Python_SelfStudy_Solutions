16c9ab30b738f9d1e453c0b2632c18646c003bc2
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-18 09:14:51 +0530

    Add files via upload

---
+++ 7_Python_FindLastElementofList.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 09:05:53 2023
+
+@author: AJINKYA
+"""
+
+#Python program to find the last element of a list
+
+#Method 1:
+    
+Ramayan = ["Ram", "Seeta", "Laxman","Hanuman", "Urmila"]
+
+print(Ramayan[-1])
+
+#Method 2:
+length_Ramayan = len(Ramayan)
+
+print(length_Ramayan)
+
+print(Ramayan[4])
